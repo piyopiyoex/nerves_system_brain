ce7ce7c76dff702664e991f6932618b56ee92fa8
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2026-09-22 21:50:23 +0900

    NervesMOTD のファームウェア情報表示とロゴを改善する

--- examples/hello_kiosk/config/config.exs
+++ examples/hello_kiosk/config/config.exs
@@ -6,6 +6,13 @@ Application.start(:nerves_bootstrap)
 # Nerves projects do. The System overlay remains responsible for PW-SH6 setup.
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
+config :nerves_motd,
+  logo: [
+    IO.ANSI.color(74),
+    File.read!(Path.expand("logo.txt", __DIR__)),
+    IO.ANSI.reset()
+  ]
+
 if Mix.target() == :host do
   import_config "host.exs"
 else
